10aeb10c90f79d235b049c55ebae20e4a7d11458
Stephen Engler <sengler1989@gmail.com>
2018-03-12 13:27:24 -0500

    button working, p tag appended with counter

--- client.js
+++ client.js
@@ -1,7 +1,26 @@
 console.log('in client');
 
+let counter = 0;
 $(document).ready(readyNow);
 
 function readyNow(){
   console.log('in jq');
+  addEventHandlers();
+}
+
+function addEventHandlers(){
+  $('#generate').on('click', makeDiv);
+}
+
+function makeDiv(){
+  counter++;
+  console.log('in make div');
+  let divContent = $('<div></div>');
+
+  divContent.append('<p>'+counter+'</p>');
+
+  $('#content').append(divContent);
+
+
+
 }
